a4c089d186a212738431d463c97de330cd2c1277
Lawrence <lawrenceshen1213@gmail.com>
2025-06-24 21:51:48 +0800

    feat: add API Getway to secure service usage

--- index.js
+++ index.js
@@ -1,33 +1,47 @@
 import * as cheerio from 'cheerio';
+import { initializeApp, applicationDefault, getApps } from 'firebase-admin/app';
+
+if (getApps().length === 0) {
+    initializeApp({
+        credential: applicationDefault(),
+    });
+}
 
 export const handler = async (req, res) => {
-  if (req.method !== 'POST') {
-    return res.status(405).json({ error: 'Method not allowed' })
-  }
+    if (req.method !== 'POST') {
+        return res.status(405).json({ error: 'Method not allowed' })
+    } 
 
-  try {
-    const { shared_link } = req.body
-    console.log('Received shared_link:', shared_link)
+    // [Optional] Verify Firebase ID token
+    const authHeader = req.headers.authorization || '';
+    const idToken = authHeader.startsWith('Bearer ') ? authHeader.split('Bearer ')[1] : null;
 
-    if (
-      !shared_link ||
-      typeof shared_link !== 'string' ||
-      !shared_link.startsWith('https://maps.app.goo.gl/')
-    ) {
-      return res.status(400).json({
-        error: 'Missing or invalid shared_link. Must start with https://maps.app.goo.gl/'
-      })
+    if (!idToken) {
+        return res.status(401).json({ error: 'Missing or invalid Authorization token' });
     }
 
-    // Optional: validate it's a valid URL
+    // Remove the code above if you don't have a API Gateway configured
+
     try {
-      new URL(shared_link)
+        const { shared_link } = req.body
+        console.log('Received shared_link:', shared_link)
+
+        if (!shared_link ||
+         typeof shared_link !== 'string' ||
+         !shared_link.startsWith('https://maps.app.goo.gl/')) {
+            return res.status(400).json({
+                error: 'Missing or invalid shared_link. Must start with https://maps.app.goo.gl/'
+            })
+        }
+
+    // Validate it's a valid URL
+    try {
+        new URL(shared_link)
     } catch {
-      return res.status(400).json({ error: 'Invalid URL format' })
+        return res.status(400).json({ error: 'Invalid URL format' })
     }
 
     const axios = (await import('axios')).default
-    // const { default: cheerio } = await import('cheerio')
 
     const expanded = await axios.get(shared_link, { maxRedirects: 5 })
     const finalUrl = expanded.request.res.responseUrl
@@ -53,9 +67,11 @@ export const handler = async (req, res) => {
     const owner_display_name = parsed?.[0]?.[3]?.[0]
     const public_saved_list_name = parsed?.[0]?.[4]
     const raw_saved_places = parsed?.[0]?.[8]
+    
     if (!Array.isArray(raw_saved_places)) {
         return res.status(500).json({ error: 'Failed to extract saved places' })
-        }
+    }
+    
     const places = raw_saved_places.map(place => {
         const loc = place[1]
         const owner = place[12]
